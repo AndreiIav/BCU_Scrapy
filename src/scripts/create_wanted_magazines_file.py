19dcc6b76f684a4ccf9853ed6ea1c9163c8ad94b
Andrei <Andrey@yahoo.com>
2024-04-09 15:31:43 +0300

    Added tests for write_wanted_magazines_file()

--- src/scripts/create_wanted_magazines_file.py
+++ src/scripts/create_wanted_magazines_file.py
@@ -13,29 +13,28 @@ path_database = Path(BASE_PATH) / DATABASE_NAME
 path_list_of_magazines_not_to_be_scrapped = (
     Path(BASE_PATH) / "extra" / "list_of_magazines_not_to_be_scrapped.txt"
 )
-
 path_wanted_magazines = Path(BASE_PATH) / "wanted_magazines.txt"
 
 
-def create_wanted_magazine_file():
+# def create_wanted_magazine_file():
 
-    already_inserted_magazine_name = get_already_inserted_magazine_name(path_database)
+already_inserted_magazine_name = get_already_inserted_magazine_name(path_database)
 
-    not_wanted_magazines = get_not_wanted_magazines(
-        path_list_of_magazines_not_to_be_scrapped
-    )
+not_wanted_magazines = get_not_wanted_magazines(
+    path_list_of_magazines_not_to_be_scrapped
+)
 
-    all_magazine_names_from_start_page = get_all_magazine_names_from_start_page(
-        START_URL_BCU, MAGAZINE_LINKS_REGEX
-    )
+all_magazine_names_from_start_page = get_all_magazine_names_from_start_page(
+    START_URL_BCU, MAGAZINE_LINKS_REGEX
+)
 
-    write_wanted_magazines_file(
-        all_magazine_names_from_start_page,
-        already_inserted_magazine_name,
-        not_wanted_magazines,
-        path_wanted_magazines,
-    )
+write_wanted_magazines_file(
+    all_magazine_names_from_start_page,
+    already_inserted_magazine_name,
+    not_wanted_magazines,
+    path_wanted_magazines,
+)
 
 
-if __name__ == "__main__":
-    create_wanted_magazine_file()
+# if __name__ == "__main__":
+#     create_wanted_magazine_file()
