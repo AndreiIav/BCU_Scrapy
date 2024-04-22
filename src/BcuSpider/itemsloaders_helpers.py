156f31ed82affe8d3fc7f9611bea4e0ac3ef60ff
Andrei <Andrey@yahoo.com>
2024-04-22 09:03:30 +0300

    Removed superflous comments from itemsloaders_helpers.py

--- src/BcuSpider/itemsloaders_helpers.py
+++ src/BcuSpider/itemsloaders_helpers.py
@@ -10,43 +10,42 @@ class IncrementId:
 
     @classmethod
     def increment_on_call(cls, y=1):
+
         cls.counter += y
         return cls.counter
 
 
 def remove_last_element_from_url(url):
+
     partition = url.rpartition("/")
+
     return partition[0] + partition[1]
 
 
 def write_to_database(database_path, table, *values):
+
     conn = sqlite3.connect(database_path)
-    conn.execute("PRAGMA foreign_keys = 1")  # to enable foreign keys
     c = conn.cursor()
 
     with conn:
-        # magazines table
         if table == "magazines":
             c.execute(
                 "INSERT INTO magazines(name, magazine_link) VALUES(?,?)",
                 (values[0], values[1]),
             )
 
-        # magazine_year table
         if table == "magazine_year":
             c.execute(
                 "INSERT INTO magazine_year(magazine_id, year, magazine_year_link) VALUES (?, ?, ?)",
                 (values[0], values[1], values[2]),
             )
 
-        # magazine_number table
         if table == "magazine_number":
             c.execute(
                 "INSERT INTO magazine_number(magazine_year_id, magazine_number, magazine_number_link) VALUES (?, ?, ?)",
                 (values[0], values[1], values[2]),
             )
 
-        # magazine_number_content
         if table == "magazine_number_content":
             c.execute(
                 "INSERT INTO magazine_number_content(magazine_number_id, magazine_content, magazine_page) VALUES(?, ?, ?)",
@@ -60,18 +59,15 @@ def get_id_from_database(database_path, table, *values):
     conn = sqlite3.connect(database_path)
     c = conn.cursor()
 
-    # magazines table
     if table == "magazines":
         c.execute("SELECT id FROM magazines WHERE name = ?", (values[0],))
 
-    # magazine_year table
     if table == "magazine_year":
         c.execute(
             "SELECT id FROM magazine_year WHERE magazine_id = ? AND year = ?",
             (values[0], values[1]),
         )
 
-    # magazine_number table
     if table == "magazine_number":
         c.execute(
             "SELECT id FROM magazine_number WHERE magazine_year_id = ? AND magazine_number = ?",
