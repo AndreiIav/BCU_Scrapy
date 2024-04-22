b566ba379a1314ac6cbe0209a2e43c1433867dda
Andrei <Andrey@yahoo.com>
2024-04-22 08:40:52 +0300

    Updated create_db_tables fixtture to create all tables

--- tests/conftest.py
+++ tests/conftest.py
@@ -203,12 +203,33 @@ def create_db_tables(create_test_tb):
     c = conn.cursor()
 
     with conn:
-        c.execute(
+        c.executescript(
             """
             CREATE TABLE magazines(
             id integer PRIMARY KEY ,
             name text,
             magazine_link text);
+
+            CREATE TABLE magazine_year(
+            id integer PRIMARY KEY,
+            magazine_id integer,
+            year text,
+            magazine_year_link text,
+            FOREIGN KEY(magazine_id) REFERENCES magazines(id));
+
+            CREATE TABLE magazine_number(
+            id integer PRIMARY KEY,
+            magazine_year_id integer,
+            magazine_number text,
+            magazine_number_link text,
+            FOREIGN KEY(magazine_year_id) REFERENCES magazine_year(id));
+
+            CREATE TABLE magazine_number_content(
+            id integer PRIMARY KEY,
+            magazine_number_id integer,
+            magazine_content text,
+            magazine_page id,
+            FOREIGN KEY(magazine_number_id) REFERENCES magazine_number(id));
             """
         )
 
