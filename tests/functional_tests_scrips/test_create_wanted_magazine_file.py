f36b4535ac3468fb06f1a74a56e9709f069b8914
Andrei <Andrey@yahoo.com>
2024-04-25 11:19:44 +0300

    Updated a fixture name in test_create_wanted_magazine_file.py

--- tests/functional_tests_scrips/test_create_wanted_magazine_file.py
+++ tests/functional_tests_scrips/test_create_wanted_magazine_file.py
@@ -9,15 +9,13 @@ from scripts.scripts_config import MAGAZINE_LINKS_REGEX
 
 def test_create_wanted_magazine_file_script(
     add_data_to_magazines_table,
-    add_data_to_not_wanted_magazines_test_file,
+    add_data_to_empty_test_file,
     tmp_path,
     mock_get_all_magazine_names_from_start_page_success_response,
 ):
 
     path_database, _ = add_data_to_magazines_table
-    path_list_of_magazines_not_to_be_scrapped, _ = (
-        add_data_to_not_wanted_magazines_test_file
-    )
+    path_list_of_magazines_not_to_be_scrapped, _ = add_data_to_empty_test_file
     path_wanted_magazines = tmp_path / "wanted_magazines.txt"
 
     already_inserted_magazine_name = get_already_inserted_magazine_name(path_database)
