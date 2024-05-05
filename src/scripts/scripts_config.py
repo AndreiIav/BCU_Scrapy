dc7483f9e364dc57941bbc903f3908c9c36c23e2
Andrei <Andrey@yahoo.com>
2024-05-05 13:01:01 +0300

    Updated scripts_config.py to use environment variables

--- src/scripts/scripts_config.py
+++ src/scripts/scripts_config.py
@@ -1,5 +1,12 @@
-BASE_PATH = r"D:\IT projects\BCU_Scrapy_Scrapper"
-DATABASE_NAME = "test.db"
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BASE_PATH = os.getenv("BASE_PATH")
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+
 START_URL_BCU = "https://documente.bcucluj.ro/periodice.html"
 # Regex for extracting the correct links of the magazines found in the start page.
 MAGAZINE_LINKS_REGEX = r"web/bibdigit/periodice/(.)+$"
