881129e3b9730745381c13f54d477143e56c4267
Andrei <Andrey@yahoo.com>
2025-01-06 13:30:04 +0200

    Updated scripts_config to have default values for BASE_PATH and DATABASE_NAME.

--- src/scripts/scripts_config.py
+++ src/scripts/scripts_config.py
@@ -1,12 +1,7 @@
-import os
+from pathlib import Path
 
-from dotenv import load_dotenv
-
-load_dotenv()
-
-BASE_PATH = os.getenv("BASE_PATH")
-DATABASE_NAME = os.getenv("DATABASE_NAME")
-
+BASE_PATH = Path(__file__).resolve().parent.parent.parent
+DATABASE_NAME = "app.db"
 START_URL_BCU = "https://documente.bcucluj.ro/periodice.html"
-# Regex for extracting the correct links of the magazines found in the start page.
+# Regex for extracting the correct links of the magazines found on START_URL_BCU
 MAGAZINE_LINKS_REGEX = r"web/bibdigit/periodice/(.)+$"
